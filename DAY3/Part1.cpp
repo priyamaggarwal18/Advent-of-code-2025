9ee1ff1768fc9a6d8921896e0d31ff9ab8e572c7
Priyam Aggarwal <122437027+priyamaggarwal18@users.noreply.github.com>
2025-12-03 21:49:02 +0530

    Added the solutions of Day 3 Advent of code  in cpp

---
+++ DAY3/Part1.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string pickBest(const string &s, int k) {
+    int n = s.size();
+    int remove = n - k;
+    vector<char> st;
+
+    for (char c : s) {
+        while (!st.empty() && remove > 0 && st.back() < c) {
+            st.pop_back();
+            remove--;
+        }
+        st.push_back(c);
+    }
+
+    while (remove > 0) {
+        st.pop_back();
+        remove--;
+    }
+
+    string res;
+    for (int i = 0; i < k; i++) res.push_back(st[i]);
+    return res;
+}
+
+int main() {
+    ifstream input("input.txt");
+    long long total = 0;
+    string s;
+
+    while (getline(input, s)) {
+        if (s.empty()) continue;
+
+        string best = pickBest(s, 2);
+
+        long long val = stoll(best);
+        total += val;
+    }
+
+    cout << "Total output joltage: " << total << endl;
+    return 0;
+}
